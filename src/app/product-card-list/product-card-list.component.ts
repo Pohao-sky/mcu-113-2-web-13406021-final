24088c333a2053d53f182084bb801cb3347e1381
Pohao <pohao121@gmail.com>
2025-04-15 19:20:56 +0800

    feat(product): 依產品清單顯示實作

--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,5 +1,5 @@
 import { Product } from './../models/product';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
 
 @Component({
@@ -9,22 +9,6 @@ import { ProductCardComponent } from '../product-card/product-card.component';
   styleUrl: './product-card-list.component.scss',
 })
 export class ProductCardListComponent {
-  product = new Product({
-    id: 1,
-    name: '書籍A',
-    authors: '作者甲、作者乙、作者丙',
-    company: '博碩文化',
-
-    isShow: true,
-
-    photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-
-    createDate: new Date('2025/4/9'),
-
-    price: 10000,
-  });
-
-  onSetDisplay(isShow: boolean): void {
-    this.product.isShow = isShow;
-  }
+  @Input()
+  products!: Product[];
 }
