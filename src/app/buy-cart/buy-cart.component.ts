93f9429422e232da6b3b1746b11c1260cc40e378
Pohao <pohao121@gmail.com>
2025-05-07 00:38:34 +0800

    feat(buy-cart): 新增購物車頁面實作

---
+++ src/app/buy-cart/buy-cart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buy-cart',
+  imports: [],
+  templateUrl: './buy-cart.component.html',
+  styleUrl: './buy-cart.component.scss'
+})
+export class BuyCartComponent {
+
+}
