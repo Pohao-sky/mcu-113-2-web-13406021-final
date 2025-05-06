15425256bb4ab495071e11e74607118de4e43eaf
Pohao <pohao121@gmail.com>
2025-05-07 00:58:46 +0800

    feat(roter): 切換產品清單頁面實作

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { BuyCartComponent } from './buy-cart/buy-cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'products', component: ProductPageComponent },
+  { path: 'buy', component: BuyCartComponent },
+];
