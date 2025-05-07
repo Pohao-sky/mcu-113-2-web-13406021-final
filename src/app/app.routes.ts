fc0efbe6d8456dc67bec90fa4d593b482992cf4e
Pohao <pohao121@gmail.com>
2025-05-07 14:31:30 +0800

    feat(product-detail): 切換產品明細頁面實作

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { BuyCartComponent } from './buy-cart/buy-cart.component';
+import { ProductDetailPageComponent } from './product-detail-page/product-detail-page.component';
 
 export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'products' },
   { path: 'products', component: ProductPageComponent },
+  { path: 'product/view/:id', component: ProductDetailPageComponent },
   { path: 'buy', component: BuyCartComponent },
 ];
--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,5 +1,5 @@
 import { Product } from './../models/product';
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { PaginationComponent } from '../pagination/pagination.component';
 
@@ -11,6 +11,8 @@ import { PaginationComponent } from '../pagination/pagination.component';
 })
 export class ProductCardListComponent {
   readonly products = input<Product[]>([]);
+
+  readonly view = output<Product>();
 
   pageIndex = 1;
 }
