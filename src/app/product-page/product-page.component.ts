fde035acf4735e46abb839e2c472fa26a294713b
Pohao <pohao121@gmail.com>
2025-05-14 15:58:13 +0800

    feat(product): 實作產品資料分頁作業

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -3,25 +3,43 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { Product } from '../models/product';
 import { Router } from '@angular/router';
+import { PaginationComponent } from '../pagination/pagination.component';
 
 @Component({
   selector: 'app-product-page',
-  imports: [ProductCardListComponent],
+  imports: [PaginationComponent, ProductCardListComponent],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss',
 })
 export class ProductPageComponent implements OnInit {
   private router = inject(Router);
 
-  private ProductService = inject(ProductService);
+  private productService = inject(ProductService);
+
+  pageIndex = 1;
+
+  pageSize = 5;
+
+  totalCount = 0;
 
   products: Product[] = [];
 
   ngOnInit(): void {
-    this.products = this.ProductService.getList();
+    this.getProducts();
   }
 
   onView(product: Product): void {
     this.router.navigate(['product', 'view', product.id]);
+  }
+
+  onPageIndexChange(pageIndex: number): void {
+    this.pageIndex = pageIndex;
+    this.getProducts();
+  }
+
+  private getProducts(): void {
+    const { data, count } = this.productService.getList(undefined, this.pageIndex, this.pageSize);
+    this.products = data;
+    this.totalCount = count;
   }
 }
