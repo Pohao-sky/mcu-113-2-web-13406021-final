1e2e6ffb6dd5034ab79765974e2fa5d087225fad
Pohao <pohao121@gmail.com>
2025-06-02 15:12:04 +0800

    feat(resolver): 建立產品資料預載資料

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { BuyCartComponent } from './buy-cart/buy-cart.component';
 import { ProductDetailPageComponent } from './product-detail-page/product-detail-page.component';
+import { productResolver } from './resolver/product.resolver';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'products' },
   { path: 'products', component: ProductPageComponent },
-  { path: 'product/view/:id', component: ProductDetailPageComponent },
+  { path: 'product/view/:id', component: ProductDetailPageComponent, resolve: { product: productResolver } },
   { path: 'buy', component: BuyCartComponent },
 ];
