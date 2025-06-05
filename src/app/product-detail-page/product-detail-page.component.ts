43bd58e30c0ba218079ed24cfacc66c2158dcadd
Pohao <pohao121@gmail.com>
2025-06-05 13:27:44 +0800

    feat(product-detail): 產品單一細項特價顯示

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,12 +1,12 @@
-import { CurrencyPipe } from '@angular/common';
-import { Component, inject, input, numberAttribute, OnInit } from '@angular/core';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { Component, inject, input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from '../models/product';
 import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-detail-page',
-  imports: [CurrencyPipe],
+  imports: [CommonModule, CurrencyPipe],
   templateUrl: './product-detail-page.component.html',
   styleUrl: './product-detail-page.component.scss',
 })
